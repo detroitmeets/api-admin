112c36202e992dc81604cdcf1e782a80c51cba13
Norman Witte III <norm.witte@gmail.com>
2019-03-10 01:11:53 -0500

    Eslint AirBNB

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const { get } = require('lodash');
 const app = require('./app');
 const { MONGODB_URI, PORT } = require('./config');
 
+const { log, error } = console;
+
 const opts = { colors: true, depth: Infinity };
 mongoose.promise = Promise;
 
-(async function () {
-    try {
-        const db = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-        console.log(inspect({ "MongoDB connected on port": get(db, 'connections.0.port', 'disconnected') }, opts));
-        const server = http.createServer(app);
-        server.listen(PORT, () => console.log(inspect({ "Server listening on port": PORT }, opts)));
-    } catch (err) {
-        console.error(inspect(err, opts));
-        process.exit(1);
-    }
+(async function bootstrap() {
+  try {
+    const db = await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    log(inspect({ 'MongoDB connected on port': get(db, 'connections.0.port', 'disconnected') }, opts));
+    const server = http.createServer(app);
+    server.listen(PORT, () => log(inspect({ 'Server listening on port': PORT }, opts)));
+  } catch (err) {
+    error(inspect(err, opts));
+    process.exit(1);
+  }
 }());
--- routes.js
+++ routes.js
@@ -5,12 +5,12 @@ const libs = require('./libs');
 
 const router = express.Router();
 
-libs.forEach(lib => {
-    router[lib.method](
-        lib.endpoint,
-        merge([bodyParser.json(), lib.middleware]),
-        (req, res) => res.status(req.data.status).json(req.data.result)
-    );
+libs.forEach((lib) => {
+  router[lib.method](
+    lib.endpoint,
+    merge([bodyParser.json(), lib.middleware]),
+    (req, res) => res.status(req.data.status).json(req.data.result),
+  );
 });
 
 module.exports = router;
